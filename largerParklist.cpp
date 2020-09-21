929e0d164a40e0ee6a772093bf3db5613c67a42d
mop03 <36209272+mop03@users.noreply.github.com>
2020-09-21 15:29:51 -0700

    Update largerParklist.cpp
    added definition

--- largerParklist.cpp
+++ largerParklist.cpp
@@ -1,1 +1,37 @@
+#include "largerParklists.h"
+ parklist::parklist()
+  {
+	  length = 0;
+  }
+  parklist::~parklist() {}
 
+  // mutators
+  void parklist::readin(ifstream &inFile) 
+  {
+	  int count;
+	  count = 0;
+	  inFile.open("file_name");
+	  while (inFile && count<AR_SIZE)
+	  {
+	  	park parkobj;
+	  	getline(inFile, namein);
+	  	getline(inFile,statein);
+	  	inFile >> visitorsin;
+	  	inFile >> acresin;
+	  	inFile.ignore(100,’\n’);
+	  	parkobj.setvalues(namein, statein, visitorsin, acresin);
+	  	array[count] = parkobj;
+	  	count ++;
+	  }
+	  inFile.close();
+  }
+  
+   <insert sorting algorithm functions>
+
+
+  //accessors
+  void parklist::displaylist() const 
+   {
+	   
+	   // are we doing this is qt? im not sure
+   }
