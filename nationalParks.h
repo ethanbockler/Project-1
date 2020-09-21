f602922bce9564cf307b39e0278d251c412d28da
mop03 <36209272+mop03@users.noreply.github.com>
2020-09-21 14:38:37 -0700

    Update nationalParks.h
    got rid of struct option, and added a class declaration

--- nationalParks.h
+++ nationalParks.h
@@ -2,14 +2,29 @@
 #include <iomanip>
 #include <string>
 using namespace std;
-// we are prob gonna need to change this but this is what i have so far.. this struct will prob have to go under a nationalpark object's private attribute
-Struct Parks // place in header, might have to replace name-acres with a nationalpark object
-{
-	string name;
-	sring state;
-	double visitors;
-	double acres;
-	Parks *next;
 
+Class park
+{
+	public:
+	//constructor and destructor 
+	park();
+	~park();
+	
+	//mutators:
+	setvalues(string name, string state, int visitors, int acres);
+	
+	// accessors 
+	string getname() const;
+	string getstate() const;
+	int getvisitors() const;
+	int getacres() const;
+	
+	
+	private: 
+	string name;
+	string state;
+	int visitors;
+	int acres;
+	
 }
 
