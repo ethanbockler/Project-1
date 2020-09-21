9feccc83fe67295fa8411824cc4a04cc3ba12c44
sdavid23 <70223193+sdavid23@users.noreply.github.com>
2020-09-21 16:57:41 -0700

    Update largerParklist.cpp

--- largerParklist.cpp
+++ largerParklist.cpp
@@ -47,3 +47,47 @@ ParkList::ParkList()
 }
 
 ParkList::~ParkList() {}
+/*************************************************************************************************************
+**************************************************************************************************************
+*basic sorting function for names/state,         
+*will adjust to adhere to class as well as style, doc guidelines/member names/etc and put into 
+*method for appropriate class
+*****************************************************************************************************************/
+
+/*int main()
+{
+	string parkName[7];
+	int index[7];
+	int i, j;
+
+	for (i = 0; i <= 6; i++)
+	{
+		cout << "Enter parkname: ";
+		getline(cin, parkName[i]);
+	}
+  
+	for (i = 0; i <= 6; i++)
+	{
+		index[i] = i;
+	}
+
+	for (i = 0; i <= 5; i++)
+	{
+		for (j = i + 1; j <= 6; j++)
+		{
+			int temp;
+			if (parkName[index[i]] > parkName[index[j]])
+			{
+				temp = index[i];
+				index[i] = index[j];
+				index[j] = temp;
+			}
+		}
+	}
+	
+
+	for (i = 0; i < 7; i++)
+	{
+		cout << parkName[index[i]] << endl;
+	}
+}*/
