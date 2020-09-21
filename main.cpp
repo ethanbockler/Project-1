09ca4f52eccbcab12fb47ca2231ad7510afaf40b
mop03 <36209272+mop03@users.noreply.github.com>
2020-09-21 15:28:11 -0700

    Update main.cpp
    moved reading to  a function in largerparklist

--- main.cpp
+++ main.cpp
@@ -10,24 +10,6 @@ int visitorsin;
 int acresin;
 // put this in general header 
 const int AR_SIZE = 11; 
-// read in from input file 
-ifstream inFile;
-inFile.open("file_name");
 
-// ADD THIS TO THE CLASS DEFINITION OF THE largerParklist.cpp
-park listofparks[AR_SIZE];
-while (inFile && count<AR_SIZE)
-{
-	park parkobj;
-	getline(inFile, namein);
-	getline(inFile,statein);
-	inFile >> visitorsin;
-	inFile >> acresin;
-	inFile.ignore(100,’\n’);
-	parkobj.setvalues(namein, statein, visitorsin, acresin);
-	listofparks[count] = parkobj;
-	count ++;
-}
-inFile.close();
 
 
