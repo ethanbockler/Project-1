69f8164eea05993cc99399c8de067915a5d668f6
mop03 <36209272+mop03@users.noreply.github.com>
2020-09-21 14:54:02 -0700

    Update nationalParksi.cpp
    class definition/implementation

--- nationalParksi.cpp
+++ nationalParksi.cpp
@@ -1,1 +1,43 @@
+#include "nationalparks.h"
+
+  //constructor and destructor 
+	park::park()
+  {
+    name = " ";
+    state = " ";
+    visitors = 0;
+    acres = 0;
+    
+  }
+	park::~park() {}
+	
+	//mutators:
+	void:: park setvalues(string namein, string statein, int visitorsin, int acresin)
+  {
+    name = namein;
+    state = statein;
+    visitors = visitorsin;
+    acres = acresin;
+  }
+	
+	// accessors 
+	string park::getname() const
+  {
+    return name;
+  }
+	string park::getstate() const
+  {
+    return state;
+  }
+	int park::getvisitors() const
+  {
+    return visitors;
+  }
+	int park::getacres() const
+  {
+    return acres;
+  }
+	
+	
+	
 
