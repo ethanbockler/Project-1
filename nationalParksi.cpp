aff0e87dd26d128322efeaeebc6fdc39f28f6869
mop03 <36209272+mop03@users.noreply.github.com>
2020-09-20 21:20:50 -0700

    Update nationalParksi.cpp
    forming the initial linked list reading in from infile.. gotta change stuff prob..

--- nationalParksi.cpp
+++ nationalParksi.cpp
@@ -1,1 +1,28 @@
+//assuming struct has been created, struct called Parks
+Parks *head;
+head = NULL;
+ifstream inFile;
+int count; 
+count = 0; 
+
+// read in from input file 
+Parks *initialptr;
+initialptr = new Parks;
+while (inFile && initialptr != NULL)
+{
+	getline(inFile, initialptr->name);
+	getline(inFile,initialptr->state);
+	inFile >> initialptr->visitors;
+	inFile >> initialptr->acres;
+	inFile.ignore(100,’\n’);
+	count ++;
+	
+	initialptr->next = head; 
+	head = initialptr; 
+	initialptr = NULL; 
+	initialptr = new Parks;
+}
+delete initialptr;
+initialptr = NULL;
+inFile.close();
 
