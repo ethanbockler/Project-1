4de135cb32a62bb8eebab89054915949e0162fff
mop03 <36209272+mop03@users.noreply.github.com>
2020-09-21 15:29:01 -0700

    Update largerParklist.h
    added declaration

--- largerParklist.h
+++ largerParklist.h
@@ -1,22 +1,27 @@
 #include "nationalParks.h"
 
-Class parklist
+ Class parklist
 {
   public:
   parklist();
   ~parklist();
-  
-  // mutators
-  
-  
-  //accessors
-  
-  
-  
-  private:
- 
-  
 
+  // mutators
+  void readin(ifstream &inFile);
+   <insert sorting algorithm functions>
+
+
+  //accessors
+  void displaylist() const; // are we doing this is qt? im not sure
+
+
+  private:
+  park array[AR_SIZE];
+  int length;
 
 
 }
+
+
+
+
