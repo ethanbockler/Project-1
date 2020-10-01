93748ce2c873648d393d7f456cac17611e489271
mop03 <36209272+mop03@users.noreply.github.com>
2020-09-30 18:46:12 -0700

    Update extrasi.cpp
    added print menu

--- extrasi.cpp
+++ extrasi.cpp
@@ -1,1 +1,14 @@
-
+void displaymenu()
+{
+  cout << "Welcome to the National Parks Vacations Pamphlet! Choose an option from the menu.\n";
+  cout << "Menu:\n";
+  cout << "1. Display list of parks sorted by name\n";
+  cout << "2. Display list of parks sorted by state\n";
+  cout << "3. Display list of parks sorted by area\n";
+  cout << "4. Display list of parks sorted by number of visitors\n";
+  cout << "5. Display list of parks with total visitors\n";
+  cout << "6. Display list of parks with total acres\n";
+  cout << "7. Help\n";
+  cout << "8. Contact Us\n";
+  
+}
