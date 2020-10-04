7466bef064971b04f5e4035f458147c57d73e1e9
kabirjs <70295694+kabirjs@users.noreply.github.com>
2020-10-04 14:30:10 -0700

    Update largerParklist.h

--- largerParklist.h
+++ largerParklist.h
@@ -1,6 +1,7 @@
 #ifndef LARGERPARKLIST_H
 #define LARGERPARKLIST_H
 #include "nationalPark.h"
+#include "extras.h"
 using namespace std;
 
 class ParkList
@@ -10,21 +11,20 @@ public:
   ~ParkList();
 
   // mutators
-  void createList(); // initializes list with national parks
-  void sortState();  // sorts by state alphebetical
-  void sortSize();   // sorts by size lower -> higher
-  void sortVisitors(); // sorts by visitors lower-> higher
+  void createList(int AR_SIZE); // initializes list with national parks
+  void sortName(int AR_SIZE);   // sorts by park name
+  void sortState(int AR_SIZE);  // sorts by state alphebetical
+  void sortSize(int AR_SIZE);   // sorts by size lower -> higher
+  void sortVisitors(int AR_SIZE); // sorts by visitors lower-> higher
   
-  void displayParks();
-  void displayParksByVisitors();
-  void displayParksBySize();  
+  void displayParks(int AR_SIZE);
+  void displayParksByVisitors(int AR_SIZE);
+  void displayParksBySize(int AR_SIZE);
 
 
 private:
-  nationalPark parkArray[AR_SIZE];
-  // we can get ar size by having a function in main that goes down the list of
-  // parks and then saves the number of lines there are
+  nationalPark parkArray[20];
   int length;
-}
+};
 
 #endif
