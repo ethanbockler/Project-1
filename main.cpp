2211d03866a692596ffd20c6b5fd1d0b9f5a14ec
kabirjs <70295694+kabirjs@users.noreply.github.com>
2020-10-04 14:29:32 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,61 @@
-#include "nationalparks.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "nationalPark.h"
+#include "largerParklist.h"
+#include "extras.h"
 
+using namespace std;
 
-ifstream inFile;
-int count; 
-count = 0; 
-string namein;
-string statein;
-int visitorsin;
-int acresin;
-// put this in general header 
-const int AR_SIZE = 11; 
+int main()
+{
+    int AR_SIZE = getArraySize();
+    ParkList parkArray;
+    parkArray.createList(AR_SIZE);
 
-
-
+    int input = 0;
+    do
+    {
+        displayMenu();
+        cin >> input;
+        switch(input)
+        {
+            case 1:
+                parkArray.sortName(AR_SIZE);
+                parkArray.displayParks(AR_SIZE);
+                break;
+            case 2:
+                parkArray.sortState(AR_SIZE);
+                parkArray.displayParks(AR_SIZE);
+                break;
+            case 3:
+                parkArray.sortSize(AR_SIZE);
+                parkArray.displayParks(AR_SIZE);
+                break;
+            case 4:
+                parkArray.sortVisitors(AR_SIZE);
+                parkArray.displayParks(AR_SIZE);
+                break;
+            case 5:
+                parkArray.sortVisitors(AR_SIZE);
+                parkArray.displayParksByVisitors(AR_SIZE);
+                break;
+            case 6:
+                parkArray.sortSize(AR_SIZE);
+                parkArray.displayParksBySize(AR_SIZE);
+                break;
+            case 7:
+                displayHelpMenu();
+                break;
+            case 8:
+                contactHelp();
+                break;
+            case 9:
+                maintinence();
+                break;
+            case 0:
+                cout << "\nThank you for using the program, goodbye!";
+        }
+    }
+    while(input != 0);
+}
