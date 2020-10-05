bbeae5fa72733712601d3e42681ace299452a102
mop03 <36209272+mop03@users.noreply.github.com>
2020-10-05 16:55:00 -0700

    Update nationalPark.h

--- nationalPark.h
+++ nationalPark.h
@@ -28,3 +28,37 @@ private:
 };
 
 #endif
+//mariam's
+#ifndef NATIONALPARKS_H_
+#define NATIONALPARKS_H_
+
+#include <string.h>
+using namespace std;
+
+class nationalPark
+{
+public:
+	//constructor and destructor
+	nationalPark();
+	~nationalPark();
+
+	//mutators:
+	void newPark(int place); // INPUT ARRAY NUMBER (LINE NUMBER - 1)
+	void setvalues(string name, string state, int visitors, int acres);
+
+	// accessors
+	string returnName();
+	string returnState();
+    	int returnVisitors();
+    	double returnSize();
+
+private:
+	string parkName;
+	string parkState;
+	int parkVisitors;
+	double parkSize;
+};
+
+
+
+#endif /* NATIONALPARKS_H_ */
